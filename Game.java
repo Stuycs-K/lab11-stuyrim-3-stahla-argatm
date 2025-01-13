3438907fb904341a43c3df68b6c8946c6101d8bd
Maxim Argat <maximargat@gmail.com>
2025-01-13 09:46:16 -0500

    cleaned up temporary code, fixed formatting somewhat

--- Game.java
+++ Game.java
@@ -1,17 +1,14 @@
 import java.util.*;
 public class Game
 {
-  private static final int WIDTH = 80;
-  private static final int HEIGHT = 30;
-  private static final int BORDER_COLOR = Text.BLACK;
-  private static final int BORDER_BACKGROUND = Text.WHITE + Text.BACKGROUND;
+    private static final int WIDTH = 80;
+    private static final int HEIGHT = 30;
+    private static final int BORDER_COLOR = Text.BLACK;
+    private static final int BORDER_BACKGROUND = Text.WHITE + Text.BACKGROUND;
 
     public static void main(String[] args)
     {
-        //run();
-        Text.clear();
-        allspaces();
-        drawBackground();
+        run();
     }
     public static void allspaces() // prints 80x30 spaces. this is so we can jump around the terminal in drawBG.
     {
@@ -65,9 +62,9 @@ public class Game
         Text.go(31, 0);
     }
 
-  //Display a line of text starting at
-  //(columns and rows start at 1 (not zero) in the terminal)
-  //use this method in your other text drawing methods to make things simpler.
+    //Display a line of text starting at
+    //(columns and rows start at 1 (not zero) in the terminal)
+    //use this method in your other text drawing methods to make things simpler.
     public static void drawText(String s,int startRow, int startCol)
     {
         /*>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>*/
@@ -80,29 +77,31 @@ public class Game
         // it cant be this simple, i hope this works
     }
 
-  /*Use this method to place text on the screen at a particular location.
-  *When the length of the text exceeds width, continue on the next line
-  *for up to height lines.
-  *All remaining locations in the text box should be written with spaces to
-  *clear previously written text.
-  *@param row the row to start the top left corner of the text box.
-  *@param col the column to start the top left corner of the text box.
-  *@param width the number of characters per row
-  *@param height the number of rows
-  */
-  public static void TextBox(int row, int col, int width, int height, String text){
-    /*>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>*/
-    //YOUR CODE HERE
-    /*<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<*/
-  }
+    /*Use this method to place text on the screen at a particular location.
+    *When the length of the text exceeds width, continue on the next line
+    *for up to height lines.
+    *All remaining locations in the text box should be written with spaces to
+    *clear previously written text.
+    *@param row the row to start the top left corner of the text box.
+    *@param col the column to start the top left corner of the text box.
+    *@param width the number of characters per row
+    *@param height the number of rows
+    */
+    public static void TextBox(int row, int col, int width, int height, String text)
+    {
+        /*>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>*/
+        //YOUR CODE HERE
+        /*<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<*/
+    }
 
 
 
 
     //return a random adventurer (choose between all available subclasses)
     //feel free to overload this method to allow specific names/stats.
-    public static Adventurer createRandomAdventurer(){
-      return new CodeWarrior("Bob"+(int)(Math.random()*100));
+    public static Adventurer createRandomAdventurer()
+    {
+        return new CodeWarrior("Bob"+(int)(Math.random()*100));
     }
 
     /*Display a List of 2-4 adventurers on the rows row through row+3 (4 rows max)
@@ -114,70 +113,76 @@ public class Game
     *Caffeine: 20 Mana: 10   Snark: 1
     * ***THIS ROW INTENTIONALLY LEFT BLANK***
     */
-    public static void drawParty(ArrayList<Adventurer> party,int startRow){
+    public static void drawParty(ArrayList<Adventurer> party,int startRow)
+    {
 
-      /*>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>*/
-      //YOUR CODE HERE
-      /*<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<*/
+      
     }
 
 
-  //Use this to create a colorized number string based on the % compared to the max value.
-  public static String colorByPercent(int hp, int maxHP){
-    String output = String.format("%2s", hp+"")+"/"+String.format("%2s", maxHP+"");
-    //COLORIZE THE OUTPUT IF HIGH/LOW:
-    // under 25% : red
-    // under 75% : yellow
-    // otherwise : white
-    return output;
-  }
+    //Use this to create a colorized number string based on the % compared to the max value.
+    public static String colorByPercent(int hp, int maxHP)
+    {
+        String output = String.format("%2s", hp+"")+"/"+String.format("%2s", maxHP+"");
+        //COLORIZE THE OUTPUT IF HIGH/LOW:
+        // under 25% : red
+        // under 75% : yellow
+        // otherwise : white
+        return output;
+    }
 
 
 
 
 
-  //Display the party and enemies
-  //Do not write over the blank areas where text will appear.
-  //Place the cursor at the place where the user will by typing their input at the end of this method.
-  public static void drawScreen(){
+    //Display the party and enemies
+    //Do not write over the blank areas where text will appear.
+    //Place the cursor at the place where the user will by typing their input at the end of this method.
+    public static void drawScreen()
+    {
+        drawBackground();
 
-    drawBackground();
+        //draw player party
 
-    //draw player party
+        //draw enemy party
 
-    //draw enemy party
+    }
 
-  }
+    public static String userInput(Scanner in)
+    {
+        //Move cursor to prompt location
 
-  public static String userInput(Scanner in){
-      //Move cursor to prompt location
+        //show cursor
 
-      //show cursor
+        String input = in.nextLine();
 
-      String input = in.nextLine();
+        //clear the text that was written
 
-      //clear the text that was written
+        return input;
+    }
 
-      return input;
-  }
+    public static void quit()
+    {
+        Text.reset();
+        Text.showCursor();
+        Text.go(32,1);
+    }
 
-  public static void quit(){
-    Text.reset();
-    Text.showCursor();
-    Text.go(32,1);
-  }
-
-  public static void run(){
-    //Clear and initialize
-    Text.hideCursor();
-    Text.clear();
+    public static void run()
+    {
+        //Clear and initialize
+        Text.hideCursor();
+        Text.clear();
+        allspaces();
+        drawBackground();
 
 
     //Things to attack:
     //Make an ArrayList of Adventurers and add 1-3 enemies to it.
     //If only 1 enemy is added it should be the boss class.
     //start with 1 boss and modify the code to allow 2-3 adventurers later.
-    ArrayList<Adventurer>enemies = new ArrayList<Adventurer>();
+    ArrayList<Adventurer> enemies = new ArrayList<Adventurer>();
+
     /*>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>*/
     //YOUR CODE HERE
     /*<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<*/
@@ -201,6 +206,9 @@ public class Game
     drawScreen();//initial state.
 
     //Main loop
+
+    if(true)
+        return; // <-- this just stops errors while i do everything above it
 
     //display this prompt at the start of the game.
     String preprompt = "Enter command for "+party.get(whichPlayer)+": attack/special/quit";
